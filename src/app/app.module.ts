ebba900625005329e9e1c0c43daab70b7e2ab4e5
mohammed <mabbakar77@gmail.com>
2022-12-12 15:59:15 +0100

    composant item and un partie de service item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { NavComponent } from './components/nav/nav.component';
+import { List2Component } from './components/list2/list2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    NavComponent,
+    List2Component
   ],
   imports: [
     BrowserModule,
