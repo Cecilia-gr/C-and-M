291cac5acf30db72dc742b317344b66f34e096a5
Cecilia-gr <cecilia.grene@gmail.com>
2022-12-15 15:55:02 +0100

    maj module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,11 @@ import { UserFormComponent } from './components/user-form/user-form.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavComponent } from './components/nav/nav.component';
-<<<<<<< HEAD
-import { List2Component } from './components/list2/list2.component';
-import { RegisterformComponent } from './registerform/registerform.component';
-import { ItemComponent } from './item/item.component';
-import { FormulairConnexionComponent } from './formulair-connexion/formulair-connexion.component';
-import { ModpasseComponent } from './modpasse/modpasse.component';
+import { ItemComponent } from './components/item/item.component';
 import { MotpasseObComponent } from './motpasse-ob/motpasse-ob.component';
-=======
 import { ContactComponent } from './components/contact/contact.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { FormulairConnexionComponent } from './components/formulair-connexion/formulair-connexion.component';
->>>>>>> 025a98f428263a6d1f80e7b995a7c44acd55cc75
 
 @NgModule({
   declarations: [
@@ -29,14 +22,9 @@ import { FormulairConnexionComponent } from './components/formulair-connexion/fo
     UserComponent,
     UserListComponent,
     NavComponent,
-<<<<<<< HEAD
-    List2Component,
-    RegisterformComponent,
     ItemComponent,
     FormulairConnexionComponent,
-    ModpasseComponent,
-    MotpasseObComponent
-=======
+    MotpasseObComponent,
     UserFormComponent,
     NotfoundComponent,
     RegisterComponent,
@@ -44,7 +32,6 @@ import { FormulairConnexionComponent } from './components/formulair-connexion/fo
     ContactComponent,
     CategoriesComponent,
     FormulairConnexionComponent
->>>>>>> 025a98f428263a6d1f80e7b995a7c44acd55cc75
   ],
   imports: [
     BrowserModule,
