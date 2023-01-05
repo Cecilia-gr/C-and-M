a8f673d55dab885c6a3595f0018003e00770e060
Cecilia-gr <cecilia.grene@gmail.com>
2023-01-05 17:03:07 +0100

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,6 @@ import { UserFormComponent } from './components/user-form/user-form.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavComponent } from './components/nav/nav.component';
-<<<<<<< HEAD
-import { List2Component } from './components/list2/list2.component';
-import { RegisterformComponent } from './registerform/registerform.component';
-import { ItemComponent } from './item/item.component';
-import { FormulairConnexionComponent } from './formulair-connexion/formulair-connexion.component';
-import { ModpasseComponent } from './modpasse/modpasse.component';
-import { MotpasseObComponent } from './motpasse-ob/motpasse-ob.component';
-=======
 import { ContactComponent } from './components/contact/contact.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { FormulairConnexionComponent } from './components/formulair-connexion/formulair-connexion.component';
@@ -36,21 +28,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     UserComponent,
     UserListComponent,
     NavComponent,
-<<<<<<< HEAD
-    List2Component,
-    RegisterformComponent,
-    ItemComponent,
-    FormulairConnexionComponent,
-    ModpasseComponent,
-    MotpasseObComponent
-=======
     UserFormComponent,
     NotfoundComponent,
     RegisterComponent,
     NavComponent,
     ContactComponent,
     CategoriesComponent,
-    FormulairConnexionComponent
+    FormulairConnexionComponent,
+    HomeComponent,
+    ItemComponent,
+    ItemFormComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
