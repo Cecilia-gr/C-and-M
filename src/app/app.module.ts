9cdbd070b34e86c40627aa0d1c8ebffcb8d807b2
mohammed <mabbakar77@gmail.com>
2023-01-05 17:02:19 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -13,14 +12,6 @@ import { UserFormComponent } from './components/user-form/user-form.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavComponent } from './components/nav/nav.component';
-<<<<<<< HEAD
-import { List2Component } from './components/list2/list2.component';
-import { RegisterformComponent } from './registerform/registerform.component';
-import { ItemComponent } from './item/item.component';
-import { FormulairConnexionComponent } from './formulair-connexion/formulair-connexion.component';
-import { ModpasseComponent } from './modpasse/modpasse.component';
-import { MotpasseObComponent } from './motpasse-ob/motpasse-ob.component';
-=======
 import { ContactComponent } from './components/contact/contact.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { FormulairConnexionComponent } from './components/formulair-connexion/formulair-connexion.component';
@@ -36,21 +27,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     UserComponent,
     UserListComponent,
     NavComponent,
-<<<<<<< HEAD
-    List2Component,
-    RegisterformComponent,
-    ItemComponent,
-    FormulairConnexionComponent,
-    ModpasseComponent,
-    MotpasseObComponent
-=======
     UserFormComponent,
     NotfoundComponent,
     RegisterComponent,
     NavComponent,
     ContactComponent,
     CategoriesComponent,
-    FormulairConnexionComponent
+    FormulairConnexionComponent,
+    HomeComponent,
+    ItemComponent,
+    ItemFormComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
@@ -59,9 +46,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
